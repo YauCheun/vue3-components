2717fdfbb7aedf30a9d6cba777684aeb7bd67301
YauCheun <1272125039@qq.com>
2024-11-16 11:56:05 +0800

    feat: icon组件样式完善

--- play/src/App.vue
+++ play/src/App.vue
@@ -2,17 +2,19 @@
  * @Author: YauCheun 1272125039@qq.com
  * @Date: 2024-11-07 08:23:39
  * @LastEditors: YauCheun 1272125039@qq.com
- * @LastEditTime: 2024-11-16 10:47:51
+ * @LastEditTime: 2024-11-16 11:55:19
  * @FilePath: \vue3-components\play\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
+
 <script setup lang="ts">
+import { AccessibilitySharp } from '@vicons/ionicons5'
 </script>
 
 <template>
- <z-icon :color="'red'" :size="20">hello</z-icon>
+  <z-icon :color="'yellow'" :size="20">
+    <AccessibilitySharp></AccessibilitySharp>
+  </z-icon>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
