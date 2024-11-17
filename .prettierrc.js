291c7f989e5f5f26c121e8c2d79cc4b5ba753144
YauCheun <1272125039@qq.com>
2024-11-17 14:41:12 +0800

    feat: prettier自动格式化

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: YauCheun 1272125039@qq.com
+ * @Date: 2024-11-17 14:23:49
+ * @LastEditors: YauCheun 1272125039@qq.com
+ * @LastEditTime: 2024-11-17 14:25:58
+ * @FilePath: \vue3-components\play\src\.prettierrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.export = {
+  singleQuote: true, //使用单引号
+  trailingComma: 'none',  //末尾逗号
+  semi: false, //使用分号
+  arrowParens: 'avoid', //箭头函数括号
+  endOfLine: 'auto', //结尾自动换行
+  tabWidth: 2,
+}
