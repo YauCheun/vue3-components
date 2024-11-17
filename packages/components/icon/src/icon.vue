83e78abbd9b20b7933c2a44f8096be086f19dfbd
YauCheun <1272125039@qq.com>
2024-11-17 13:13:40 +0800

    feat: 配置eslint

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -2,7 +2,7 @@
  * @Author: YauCheun 1272125039@qq.com
  * @Date: 2024-11-15 07:59:30
  * @LastEditors: YauCheun 1272125039@qq.com
- * @LastEditTime: 2024-11-15 08:19:43
+ * @LastEditTime: 2024-11-17 12:59:43
  * @FilePath: \vue3-components\packages\components\icon\src\icon.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 
